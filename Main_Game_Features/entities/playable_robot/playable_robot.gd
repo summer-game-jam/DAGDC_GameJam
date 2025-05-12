d04c23f11b468be282d52580c7a6cbdbb6faf2f5
Eric-Chrisman <118196908+Eric-Chrisman@users.noreply.github.com>
2025-05-12 15:57:17 -0500

    battery

--- Main_Game_Features/entities/playable_robot/playable_robot.gd
+++ Main_Game_Features/entities/playable_robot/playable_robot.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 class_name Playable_Robot
 
-var gravity: float = 9.8 # going down is the positive y direction
+var gravity: float = 20 # going down is the positive y direction
 
-var jump_force: float = -500
-var horizonal_move_speed: float = 30000 
+var jump_force: float = -600
+
+var max_horizonal_move_speed: float = 500
+var horizonal_move_speed: float = 2000
 
 func _physics_process(delta: float) -> void:
 	if is_on_floor():
@@ -16,9 +18,13 @@ func _physics_process(delta: float) -> void:
 		var horizonal_move_unit_vector: float = 0
 		if Input.is_action_pressed("left"):
 			horizonal_move_unit_vector -= 1
-		if Input.is_action_pressed("right"):
+		elif Input.is_action_pressed("right"):
 			horizonal_move_unit_vector += 1
-		velocity.x = delta * horizonal_move_unit_vector * horizonal_move_speed
+		else:
+			velocity.x = 0
+		velocity.x += delta * horizonal_move_unit_vector * horizonal_move_speed
+		velocity.x = clamp(velocity.x, -max_horizonal_move_speed, max_horizonal_move_speed)
+		
 	else:
 		velocity.y += gravity
 	
