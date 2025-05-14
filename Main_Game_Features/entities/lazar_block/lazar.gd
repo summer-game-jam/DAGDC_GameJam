c31f6e37a92ec423afbdab14d9e985c98cbfc649
Eric-Chrisman <118196908+Eric-Chrisman@users.noreply.github.com>
2025-05-14 07:10:42 -0500

    lazar target wip
    gtg help my dad. will fiinish later today

--- Main_Game_Features/entities/lazar_block/lazar.gd
+++ Main_Game_Features/entities/lazar_block/lazar.gd
@@ -6,6 +6,7 @@ class_name Lazar
 
 func _process(delta: float) -> void:
 	$Line2D.points[-1] = $RayCast2D.target_position
+	$Area2D.position = $RayCast2D.target_position
 
 func swap_direction(new_direction: bool) -> void:
 	direction = new_direction
--- Main_Game_Features/entities/lazar_target/lazar_target.gd
+++ Main_Game_Features/entities/lazar_target/lazar_target.gd
@@ -1,2 +1,10 @@
 extends Entity
 class_name Lazar_Target
+
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.get_parent() is Lazar:
+		print("yes")
+	else:
+		print("no:\t", area)
