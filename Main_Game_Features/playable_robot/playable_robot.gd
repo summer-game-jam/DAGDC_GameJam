d1f7b62bce812779207c92c0e0c9d75cc28696af
Eric-Chrisman <118196908+Eric-Chrisman@users.noreply.github.com>
2025-05-15 21:16:06 -0500

    block pyshics wip progress

--- Main_Game_Features/playable_robot/playable_robot.gd
+++ Main_Game_Features/playable_robot/playable_robot.gd
@@ -1,16 +1,13 @@
-extends CharacterBody2D
+extends MoveableBody
 class_name Playable_Robot
 
 signal bot_out_of_power
-
-var gravity: float = 1300 # going down is the positive y direction
 
 var jump_force: float = -600
 var air_speed_reduction: float = 0.2
 
 var max_horizonal_move_speed: float = 500
 var horizonal_move_speed: float = 2000
-var push_speed: float = max_horizonal_move_speed / 5
 
 var dead: bool = false
 
@@ -33,6 +30,7 @@ func _on_node_button_held_limit_reached() -> void:
 	$Timer.stop()
 	_on_timer_timeout()
 
+#replace me
 func update_battery() -> void:
 	$Label.text = str($Timer.time_left)
 
@@ -66,12 +64,3 @@ func move_player(delta: float) -> void:
 		velocity.y += gravity * delta
 		velocity.x += delta * horizonal_move_unit_vector * horizonal_move_speed * air_speed_reduction
 		velocity.x = clamp(velocity.x, -max_horizonal_move_speed, max_horizonal_move_speed)
-
-func handle_push_collisions(delta: float) -> void:
-	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i)
-		var other = collision.get_collider()
-		if other is CharacterBody2D:
-			var normal = collision.get_normal()
-			if abs(normal.x) > 0.5:
-				other.global_position += -normal * push_speed * delta
