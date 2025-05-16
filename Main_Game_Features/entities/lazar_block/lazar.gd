1451c2262e0155df5007223182ad9caac606450b
Eric-Chrisman <118196908+Eric-Chrisman@users.noreply.github.com>
2025-05-16 11:29:42 -0500

    some bug fixes. lazar block can move
    now working on bot  rotation and pickup and pickup rotation

--- Main_Game_Features/entities/lazar_block/lazar.gd
+++ Main_Game_Features/entities/lazar_block/lazar.gd
@@ -5,16 +5,12 @@ class_name Lazar
 @export var direction: bool = true
 
 func _process(delta: float) -> void:
-	if $RayCast2D.is_colliding():
-		$Line2D.points[-1] = to_local($RayCast2D.get_collision_point())
-		var object_lazar_hit = $RayCast2D.get_collider()
+	if $MoveableBody/RayCast2D.is_colliding():
+		$MoveableBody/Line2D.points[-1] = $MoveableBody/Line2D.to_local($MoveableBody/RayCast2D.get_collision_point())
+		var object_lazar_hit = $MoveableBody/RayCast2D.get_collider()
 		#print(object_lazar_hit)
 		if object_lazar_hit.get_parent() is Lazar_Target:
 			object_lazar_hit.get_parent()
-	if Input.is_action_pressed("left"):
-		position.x -= delta * 100
-	if Input.is_action_pressed("right"):
-		position.y -= delta * 100
 
 func swap_direction(new_direction: bool) -> void:
 	direction = new_direction
