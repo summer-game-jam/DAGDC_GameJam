4390defff167cdc9893f77e9b843dcfd813ad0cc
Eric-Chrisman <118196908+Eric-Chrisman@users.noreply.github.com>
2025-05-17 18:29:40 -0500

    pick up kinda works, need to adjust height for object pick up

--- Main_Game_Features/entities/lazar_block/lazar.gd
+++ Main_Game_Features/entities/lazar_block/lazar.gd
@@ -5,11 +5,11 @@ class_name Lazar
 @export var direction: bool = true
 
 func _process(delta: float) -> void:
-	if $MoveableBody/RayCast2D.is_colliding():
-		$MoveableBody/Line2D.points[-1] = $MoveableBody/Line2D.to_local($MoveableBody/RayCast2D.get_collision_point())
-		var object_lazar_hit = $MoveableBody/RayCast2D.get_collider()
+	if $RayCast2D.is_colliding():
+		$Line2D.points[-1] = $Line2D.to_local($RayCast2D.get_collision_point())
+		var object_lazar_hit = $RayCast2D.get_collider()
 		#print(object_lazar_hit)
-		if object_lazar_hit.get_parent() is Lazar_Target:
+		if object_lazar_hit and object_lazar_hit.get_parent() is Lazar_Target:
 			object_lazar_hit.get_parent()
 
 func swap_direction(new_direction: bool) -> void:
